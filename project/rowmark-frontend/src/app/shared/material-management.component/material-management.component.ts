1462425811adcef0c65649cb965be83a6ae1cac7
Freddy Fernandez <fafernandez.24@est.ucab.edu.ve>
2026-06-04 00:54:05 -0400

    refact: Se termino la UI de los materiales

--- project/rowmark-frontend/src/app/shared/material-management.component/material-management.component.ts
+++ project/rowmark-frontend/src/app/shared/material-management.component/material-management.component.ts
@@ -10,10 +10,10 @@ export class MaterialManagementComponent {
 
   selectedMaterial: Material = { id: 0, name: '', imgUrl: '', category: '', description: '' };
 
-  materials: Material[] = [
-    { id: 1, name: 'Material 1', category: 'Madera', description: 'Lorem ipsum dolor sit amet', imgUrl: 'https://tse2.mm.bing.net/th/id/OIP.ruDUGqmS0CNXFLH1qRWkYAHaE8?r=0&cb=thfvnextfalcon&rs=1&pid=ImgDetMain&o=7&rm=3' },
-    { id: 2, name: 'Material 2', category: 'Plástico', description: 'Lorem ipsum dolor sit amet', imgUrl: 'https://tse1.mm.bing.net/th/id/OIP.FGaiFYn85RKz39GvDNGyhgHaD3?r=0&cb=thfvnextfalcon&rs=1&pid=ImgDetMain&o=7&rm=3' },
-    { id: 3, name: 'Material 3', category: 'Metal', description: 'Lorem ipsum dolor sit amet', imgUrl: 'https://tse3.mm.bing.net/th/id/OIP.raY1I7m0vPLI3KIRq_3oDwHaEK?r=0&cb=thfvnextfalcon&rs=1&pid=ImgDetMain&o=7&rm=3' },
+    materials: Material[] = [
+    { id: 1, name: 'Fibra de Densidad Media (MDF)', category: 'Madera', description: 'La fibra de densidad media (MDF) es un material compuesto hecho de fibras de madera comprimidas y unido con adhesivos.', imgUrl: 'https://tse2.mm.bing.net/th/id/OIP.ruDUGqmS0CNXFLH1qRWkYAHaE8?r=0&cb=thfvnextfalcon&rs=1&pid=ImgDetMain&o=7&rm=3' },
+    { id: 2, name: 'Material 2', category: 'Plástico', description: 'El plástico es un material sintético ampliamente utilizado en la industria y el consumo diario, conocido por su ligereza, durabilidad y versatilidad en la fabricación de diversos productos.', imgUrl: 'https://tse1.mm.bing.net/th/id/OIP.FGaiFYn85RKz39GvDNGyhgHaD3?r=0&cb=thfvnextfalcon&rs=1&pid=ImgDetMain&o=7&rm=3' },
+    { id: 3, name: 'Material 3', category: 'Metal', description: 'El metal es un material natural, ampliamente utilizado en la industria y la construcción, conocido por su resistencia, durabilidad y capacidad de conductividad térmica y eléctrica.', imgUrl: 'https://tse3.mm.bing.net/th/id/OIP.raY1I7m0vPLI3KIRq_3oDwHaEK?r=0&cb=thfvnextfalcon&rs=1&pid=ImgDetMain&o=7&rm=3' },
   ];
 
   addMaterial(name: string): void {
