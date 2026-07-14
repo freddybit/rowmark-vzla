be7c8d77dabc963fc90843205810c36e76ed486e
Freddy Fernandez <fafernandez.24@est.ucab.edu.ve>
2026-07-14 12:17:07 -0400

    refact: Se cambio el servicio de backend de Azure a Google Cloud Run

--- project/rowmark-frontend/src/environments/environment.ts
+++ project/rowmark-frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://rowmark-vzla-apargzf7gyggdnc7.centralus-01.azurewebsites.net/api/',
+  apiUrl: 'https://rowmark-vzla-git-616985224501.europe-west1.run.app/api/',
   supabaseUrl: 'https://kmuimrleeibvimybgalv.supabase.co',
   supabaseKey: 'sb_publishable_ehu1JCtWULqcIHzNxuT8AA_HKOSba4X',
 };
