3eaaede2eedfd250852207ad8a619c726dcc6b4f
Freddy Fernandez <fafernandez.24@est.ucab.edu.ve>
2026-07-14 13:49:45 -0400

    fix: vercel output directory setup

--- project/rowmark-frontend/src/app/core/app.routes.server.ts
+++ project/rowmark-frontend/src/app/core/app.routes.server.ts
@@ -2,9 +2,22 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'admin',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'admin/:id',
+    renderMode: RenderMode.Client,
+  },
+  {
     path: 'admin/**',
     renderMode: RenderMode.Client,
   },
+  {
+    path: 'login',
+    renderMode: RenderMode.Client,
+  },
+
   {
     path: '**',
     renderMode: RenderMode.Prerender,
